2bf683246244ff5f4f95e91bde909cd1d0affa33
antvrobin <98505507+antvrobin@users.noreply.github.com>
2023-03-28 22:14:02 -0500

    Add files via upload

---
+++ phoenixGPT.py
@@ -0,0 +1,28 @@
+from objectionpy import preset, enums, assets, objection
+from objectionpy.frames import *
+
+scene = objection.Scene(objection.Options(
+    #dialogueBox = enums.PresetDialogueBox.TRILOGY,
+))
+
+def frameWithCharacterPose(characterChosen, whatTheySay, pose, bubbleChosen):
+    scene.frames.append(Frame(
+    char = FrameCharacter(
+        character=characterChosen,
+        poseSubstr=pose
+    ),
+    text = whatTheySay,
+    bubble = bubbleChosen
+))
+
+# PASTE THE CHATGPT OUTPUT UNDER HERE 
+
+
+
+
+
+
+
+
+with open('./scene.objection', 'w') as f:
+    f.write(scene.makeObjectionFile(scene.compile()))
